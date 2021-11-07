76e3d158aed2cc4bedfc3b2dafcc60c99257e416
Shivam Singhal <kas442000@gmail.com>
2021-11-07 20:20:43 +0530

    Add album to `album` table

--- data-access/main.go
+++ data-access/main.go
@@ -58,6 +58,18 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("Album found: %v\n", alb)
+
+	albId, addAlbErr := addAlbum(Album{
+		Title:  "The Modern Sound of Betty Carter",
+		Artist: "Betty Carter",
+		Price:  49.99,
+	})
+
+	if addAlbErr != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("ID of added album: %v\n", albId)
 }
 
 func albumsByArtist(name string) ([]Album, error) {
@@ -99,4 +111,16 @@ func albumByID(id int64) (Album, error) {
 	}
 
 	return alb, nil
+}
+
+func addAlbum(alb Album) (int64, error) {
+	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)", alb.Title, alb.Artist, alb.Price)
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	return id, nil
 }
