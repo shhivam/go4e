50c1d6b98d1f967bd5173ece576567c91d1b330f
Shivam Singhal <kas442000@gmail.com>
2021-11-07 19:55:03 +0530

    Query list of albums by a certain Artist

--- data-access/main.go
+++ data-access/main.go
@@ -12,13 +12,20 @@ import (
 
 var db *sql.DB
 
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
 func main() {
-	// Capture connection properties.
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
+	// Capture connection properties.
 	cfg := mysql.Config{
 		User:   os.Getenv("DBUSER"),
 		Passwd: os.Getenv("DBPASSWORD"),
@@ -40,7 +47,37 @@ func main() {
 	}
 	fmt.Println("Connected!")
 
-	// fmt.Println(os.Getenv("DBUSER"))
-	// fmt.Println(os.Getenv("DBPASSWORD"))
+	albums, queryErr := albumsByArtist("John Coltrane")
+	if queryErr != nil {
+		log.Fatal(err)
+	}
 
+	fmt.Println("Albums found: %v\n", albums)
+}
+
+func albumsByArtist(name string) ([]Album, error) {
+	var albums []Album
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+
+	defer rows.Close()
+
+	// Loop through rows, using Scan to assign column data to struct fields
+	for rows.Next() {
+		var alb Album
+		// rows.Scan is kind of strange function.
+		// It assumes that you know the sequence of columns i.e.
+		// you know that `xyz` column will come after `abc` column
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+
+	return albums, nil
 }
