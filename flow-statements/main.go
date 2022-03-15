e7a7a34da988a1f15a4b254fec6f2a304d2d1241
Shivam <kas442000@gmail.com>
2022-03-15 18:26:40 +0530

    Practice closures & maps

--- flow-statements/main.go
+++ flow-statements/main.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func fibonacci() func() int {
+	last, lastSecond := 0, 1
+	return func() int {
+		nextFib := last + lastSecond
+		lastSecond = last
+		last = nextFib
+		return nextFib
+	}
+}
+
+func fiboMain() {
+	// Practicing closures
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
+
+// Practicing maps
+func WordCount(s string) map[string]int {
+	splitWords := strings.Split(s, " ")
+	var frequencies map[string]int = make(map[string]int)
+
+	for _, wordStr := range splitWords {
+		if count, ok := frequencies[wordStr]; ok {
+			frequencies[wordStr] = count + 1
+		} else {
+			frequencies[wordStr] = 1
+		}
+	}
+
+	return frequencies
+}
+
+func main() {
+	fmt.Println(WordCount("shivam is at at nilenso"))
+	fiboMain()
+}
