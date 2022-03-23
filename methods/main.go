a4ac08653208eb5b751d66297e8a64dc94e25c29
Shivam <kas442000@gmail.com>
2022-03-23 15:08:16 +0530

    Try a stringer method

--- methods/main.go
+++ methods/main.go
@@ -13,9 +13,14 @@ func (v Vertex) Abs() float64 {
 	return v.X + v.Y
 }
 
+func (v Vertex) String() string {
+	return fmt.Sprintf("X: %v \nY: %v", v.X, v.Y)
+}
+
 func main() {
 	myV := Vertex{12, 12}
 	fmt.Println(myV.Abs())
 
+	fmt.Println(myV)
 	interfaces.MainFunc()
 }
